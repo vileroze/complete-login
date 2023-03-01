49d33976574cc0937b4ea0d37dabf38f39d37f81
vileroze <50352881+vileroze@users.noreply.github.com>
2023-03-01 13:36:39 +0545

    code cleanup

--- frontend/cl-linkedin.php
+++ frontend/cl-linkedin.php
@@ -1,61 +1,55 @@
 <?php
 
-//STEP: 1
-//get the 'code' from url
+/**
+ * STEP: 1
+ * get the 'code' from url
+ */
+
 $first_code = empty($_GET['code']) ? '' : $_GET['code'];
 if ($first_code != '') {
 
     $first_code = trim($first_code," ");
-    $user_firstname = '';
-    $user_lastname = '';
+    $user_name = '';
     $user_email = '';
 
-    ?>
-        <script>
-            //print code
-            // console.log("CODE== <?php //echo $first_code; ?>");
-        </script>
-    <?php
 
-    //STEP: 2
-    //pass it into the below url to get access token
+    /**
+     * STEP: 2
+     * pass it into the below url to get access token
+     */
+
     $url = "https://www.linkedin.com/oauth/v2/accessToken?code=".$first_code."&grant_type=authorization_code&client_id=86q3eul364itg6&client_secret=Tev6wxO3NnTVnLAH&redirect_uri=https://scary-novel.localsite.io/";
     $json = file_get_contents($url);
     $json_data = json_decode($json, true);
     $linkedin_access_token = $json_data["access_token"];
 
-    //print the access token
-    ?>
-    <script>
-        // console.log("ACCESS TOKEN== <?php //echo $linkedin_access_token; ?>");
-    </script>
-    <?php
 
-    //STEP: 3
-
-    //pass the above access token below as a param to get user info
+    /**
+     * STEP: 3
+     * pass the above access token below as a param to get user info
+     * gets both name and email separately
+     */
 
     //gets basic user info (excluding email)
     $url_basic = 'https://api.linkedin.com/v2/me';
 
-    $additional_headers = [                                                                        
-        'Authorization: Bearer '.$linkedin_access_token.'',
-    ];
-
     $ch_user_info = curl_init($url_basic);                                                                      
     curl_setopt($ch_user_info, CURLOPT_CUSTOMREQUEST, "GET");                                                                     
-    curl_setopt($ch_user_info, CURLOPT_HTTPHEADER, $additional_headers); 
+    // curl_setopt($ch_user_info, CURLOPT_HTTPHEADER, $additional_headers); 
+    curl_setopt($ch_user_info, CURLOPT_HTTPHEADER, [
+        'Authorization: Bearer '. $linkedin_access_token,
+        'X-Restli-Protocol-Version: 2.0.0',
+        'Accept: application/json',
+        'Content-Type: application/json'
+      ]);
+      curl_setopt($ch_user_info, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch_user_info, CURLOPT_VERBOSE, 1);
+      curl_setopt($ch_user_info, CURLOPT_HEADER, 1);
 
-    $server_output = curl_exec ($ch_user_info); //gets basic profile
-
-    ?>
-        <script>
-            
-            var user_info = JSON.stringify(<?php echo $server_output; ?>); 
-            // console.info("==USER INFO== " + user_info); 
-            console.log((<?php print_r($server_output); ?>));
-        </script>
-    <?php
+    $basic_response = curl_exec($ch_user_info);
+    $basic_headerSize = curl_getinfo($ch_user_info, CURLINFO_HEADER_SIZE);
+    $basic_body = substr($basic_response, $basic_headerSize);
+ 
 
     //get email address of user
     $url_for_email = 'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))';
@@ -72,28 +66,27 @@ if ($first_code != '') {
     curl_setopt($ch_user_email, CURLOPT_VERBOSE, 1);
     curl_setopt($ch_user_email, CURLOPT_HEADER, 1);
 
-    $response = curl_exec($ch_user_email);
-    $headerSize = curl_getinfo($ch_user_email, CURLINFO_HEADER_SIZE);
-    $body = substr($response, $headerSize);
-    $response_body = json_decode($body,true);
+    $email_response = curl_exec($ch_user_email);
+    $email_headerSize = curl_getinfo($ch_user_email, CURLINFO_HEADER_SIZE);
+    $email_body = substr($email_response, $email_headerSize);
+    $email_response_body = json_decode($email_body,true);
 
-    //print the email address
+
+    //print all user info (name and email)
+    $user_email = (($email_response_body['elements'][0])['handle~'])['emailAddress'];
+    $user_name = $basic_response_body["localizedFirstName"] . ' ' .$basic_response_body["localizedLastName"];
     ?>
-    <script>
-        console.log("==EMAIL ADDR==");
-        console.log((<?php print_r($response_body); ?>));
-        console.log(
-            <?php 
-                print_r((($response_body['elements'][0])['handle~'])['emailAddress']); 
-            ?>);
-    </script>
+        <script>
+            console.log("=========== Linkedin User Info ===========");
+            console.log("Linkedin name =  <?php echo $user_name;  ?>");
+            console.log("Linkedin email =  <?php echo $user_email;  ?>");
+        </script>
     <?php
 
     
 }else{
 
-    ?>
-        <script>console.log("======= <?php echo 'code aako chaina'; ?>"); </script>
-    <?php
+    echo '<script>console.log("=======code aako chaina"); </script>';
+
 }
 
